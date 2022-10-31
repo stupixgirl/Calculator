4591fa4454c938b094ab535f5867129ba6cdd5ed
stupixgirl <ldtt.thao@gmail.com>
2022-10-31 16:30:56 +0700

    Tester

---
+++ CalculatorTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTester")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9813dbb3-6fd0-49bb-91dc-5bfe1704f834")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalculatorTester/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Calculator;
+
+namespace CalculatorTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public Calculation cal;
+        [TestInitialize]
+        public void setup()
+        {
+            this.cal = new Calculation(10, 5);
+        }
+        [TestMethod]
+        public void TestAddOperation()
+        {
+            Assert.AreEqual(cal.Execute("+"), 15);
+        }
+        [TestMethod]
+        public void TestSubOperation()
+        {
+            Assert.AreEqual(cal.Execute("-"), 5);
+        }
+        [TestMethod]
+        public void TestMulOperation()
+        {
+            Assert.AreEqual(cal.Execute("*"), 50);
+        }
+        [TestMethod]
+        public void TestDivOperation()
+        {
+            Assert.AreEqual(cal.Execute("/"), 2);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestDivByZero()
+        {
+            Calculation c = new Calculation(2,0);
+            c.Execute("/");
+        }
+     /* [TestMethod]
+           public void TestDivRound()
+         {
+             Calculation c = new Calculation(5, 3);
+             Assert.AreEqual(c.Execute("/"), 2); 
+         }*/
+
+    }
+}
